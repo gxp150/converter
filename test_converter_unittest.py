9975d402992e0fbb7e99eacec461706cb5ca5e5a
GXP150 <79392623+gxp150@users.noreply.github.com>
2021-03-08 09:45:30 +1100

    Update test_converter_unittest.py
    Update with TypeError

--- test_converter_unittest.py
+++ test_converter_unittest.py
@@ -1,3 +1,4 @@
+import pytest
 import unittest
 from converter import fahrenheit
 
@@ -8,5 +9,11 @@ class TestCelsius(unittest.TestCase):
         self.assertAlmostEqual(fahrenheit(-4),(-4)*(9/5)+32)
         self.assertRaises(TypeError, fahrenheit, True)
         self.assertRaises(TypeError, fahrenheit, "34")
+        
+def testing_a_random_TypeError():
+    """Testing a random TypeError."""
+    with pytest.raises(TypeError):
+        TypeError = "Must be fahrenheit"
+        TypeError = "Must be 34"
 
 unittest.main()
